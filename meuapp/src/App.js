53a1fde6edc06908ce4d14eabfc90a9a1198b3d5
Antonio Fernando <jufer376@gmail.com>
2025-03-01 21:10:50 -0300

    Renderização condicional

--- meuapp/src/App.js
+++ meuapp/src/App.js
@@ -4,14 +4,56 @@ import Membro from './components/Membro/Membro';
 
 class App extends Component{
 
+  constructor(props){
+    super(props);
+    this.state = {
+      status: true
+
+    }
+    this.entrar = this.entrar.bind(this)
+    this.sair = this.sair.bind(this)
+
+  }
+
+  entrar(){
+
+    this.setState({status: true})
+  }
+
+  sair(){
+
+    this.setState({status: false})
+  }
+
   render(){
     return(
 
       <div>
 
-        <h1>
-            <Membro nome = "Matheus" />
-        </h1>
+          {this.state.status ? 
+          <div>
+
+            <h2> Bem vindo ao sistema</h2>
+            <button onClick={this.sair}>
+
+                  Login
+            </button>
+
+          </div> : 
+          <div> 
+              <h2>
+                Olá visitante, faça o login!
+              </h2>
+            <button onClick={this.entrar}> Entrar no sistema</button>
+            
+             </div>
+            }
+          
+          
+          
+          
+            
+           
 
       </div>
 
