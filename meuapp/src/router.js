65edb997133aac0116b9cbb5bf7b4b489187e516
Antonio Fernando <jufer376@gmail.com>
2025-04-07 21:01:00 -0300

    Crie uma rota coringa com o '*'

--- meuapp/src/router.js
+++ meuapp/src/router.js
@@ -3,6 +3,9 @@ import Header from "./components/Header/index.js";
 import Home from './pages/home/Home.js'
 import Sobre from './pages/sobre/index.js'
 import Contato from './pages/Contato/index.js'
+import Erro from '.pages/error/index.js'
+
+
 
 function RouterApp(){
     return(
@@ -16,7 +19,7 @@ function RouterApp(){
                 <Route path="/sobre" element = {<Sobre/>}  />
                 <Route path='/Contato' element = {<Contato/>} />
 
-                
+                <Route path='*' element = {<Erro/>} />
             </Routes>
 
         </BrowserRouter>
